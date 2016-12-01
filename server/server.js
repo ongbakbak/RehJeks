b6d63469453493bbdae089bb3cef9dc6319622bb
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-01 15:56:30 -0800

    Provided functionality for basic server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+// require in routes
+// requre in database ??? Or modulize elsewhere
+
+var app = express();
+
+require('./utils/routes.js')(app, express);
+
+app.use(bodyparser.json());
+app.use(express.static('/../client'));
+
+app.listen(8000);
+console.log("Listening on 127.0.0.1:8000")
