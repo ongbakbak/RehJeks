7b0b6fff842daa9cf81e263a538df061e60bce4a
Michael Thelen <michael.thelen@gmail.com>
2016-12-02 10:51:11 -0800

    renamed all signUp to signup. CamelCase Fix

--- client/signup/signup.js
+++ client/signup/signup.js
@@ -1,7 +1,10 @@
-angular.module('rehjeks.signUp',[])
-  .controller('SignUpController', function ($scope, Auth){
+angular.module('rehjeks.signup',[])
+  .controller('SignupController', function ($scope, Auth){
     $scope.user = {};
-    $scope.signUp = function() {
-      Auth.signUp($scope.user);
+    $scope.signup = function() {
+      console.log("client invoking signUp");
+      console.log("$scope.user = ", $scope.user);
+
+      Auth.signup($scope.user);
     };
   });
