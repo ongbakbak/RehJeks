5bc44c48c4ba6f8c80c82f67a6b19f190a4bb4d0
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-05 09:30:21 -0800

    Working on user methods

--- server/utils/users/userController.js
+++ server/utils/users/userController.js
@@ -3,18 +3,52 @@ var User = require('./userModel');
 
 module.exports.getSolvedChallenges = function() {
   console.log('getSolvedChallenges')
-  
+ 	
 };
 
 
-module.exports.signup = function(req, res) {
-  console.log('Server got POST @ /signup');
-  console.log("req.body = ", req.body);
+module.exports.signup = function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password; 
 
+  User.findOne({username: username})
+  .then(function(user) {
+    if(user) {
+      next(new Error("username already exists"));
+    }
+    else {
+      User.create({
+        username: username, 
+        password: password
+      })
+      //send token here
+      res.send(201);
+    }
+  })
 
 };
 
-module.exports.login = function() {
-  console.log('login')
+module.exports.login = function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
 
+  User.findOne({username: username})
+    .then(function(user){
+    	if(user){
+    		if(user.pw === password){
+          //send token here
+          res.send(200);
+    		}
+        else {
+          next(new Error("password is incorrect"));
+        }
+    	}
+    	else {
+    		next(new Error('user does not exist'));
+    	}
+    })
 };
+
+
+// check if user in database can login
+
