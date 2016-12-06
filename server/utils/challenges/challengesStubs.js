e04fa12d38bf2d7bc363fb34d8d71172bf23f324
Tenzin Sonam <tenso2006@gmail.com>
2016-12-06 13:27:23 -0800

    Created: challengeStubs

---
+++ server/utils/challenges/challengesStubs.js
@@ -0,0 +1,63 @@
+var challengeArray = [{
+  "title": "Hex Color",
+  "prompt": "write a regular expression that matches a hex color",
+  "text": ["#abc", "#f00", "#BADA55", "#C0FFEE"],
+  "difficulty": "hard",
+  "expected": [""],
+  "answer": "/^#([a-f\d]{3}){1,2}$i",
+  "cheats": [""]
+},
+{
+  "title": "Number",
+  "prompt": "Number without exponent or digit separators",
+  "text": ["-1", ".05", "+1000", "3.1415926535", "42."],
+  "difficulty": "hard",
+  "expected": [""],
+  "answer": "/^[-+]?(\d*\.?\d+|\d+\.)$/",
+  "cheats": [""]
+},
+{
+  "title": "ISO 8601 Dates",
+  "prompt": "Just dates, no time or timezone information",
+  "text": ["2012-12-12", "1986-06-13"],
+  "difficulty": "very hard",
+  "expected": [""],
+  "answer": "/^\d{4}-(0\d|1[0-2])-([0-2]\d|3[01])$/",
+  "cheats": [""]
+},
+{
+  "title": "Intersection",
+  "prompt": "A 6+ letter password with at least: one number, one letter and a symbol",
+  "text": "",
+  "difficulty": "hard",
+  "expected": [""],
+  "answer": "/^(?=.*\d)(?=.*[a-z])(?=.*[W_]).{6,}$/i",
+  "cheats": [""]
+},
+{
+  "title": "Word Count",
+  "prompt": "Count words",
+  "text": ["a word to count"],
+  "difficulty": "medium",
+  "expected": ["4"],
+  "answer": "text[0].split(/\s+/)",
+  "cheats": [""]
+},
+{
+  "title": "Subtraction",
+  "prompt": "Any number that's not divisible by 50",
+  "text": ["50"],
+  "difficulty": "medium",
+  "expected": [""],
+  "answer": "/\b(?!\d+[50]0)\d+\b/",
+  "cheats": [""]
+},
+{
+  "title": "Negation",
+  "prompt": "write a regular expression that matches a hex color",
+  "text": ["foo", "'bar'", "'He said "hi"'", '"He 'said''],
+  "difficulty": "very hard",
+  "expected": [""],
+  "answer": (/("|')(\\?.)*?\1/g),
+  "cheats": [""]
+}]
