658508e65c387c724a8f476183f7b142e6e02b75
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-07 16:50:23 -0800

    Authentication functionality completed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var stub = require('./utils/databaseStub');
 var routes = require('./utils/routes.js');
+var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -10,12 +11,24 @@ var db = mongoose.connect('mongodb://127.0.0.1:27017/rehjeks');
 
 //stub(db);
 
-
-
 var app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
+
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport config
+var User = require('./utils/users/userModel');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 routes(app, express);
 
