6e33634d3007276f09b4039988dafe4f22d2a935
Nate Hackman <nathan.hackman@gmail.com>
2016-12-08 23:33:15 -0800

    Updated Clock, Added CSS

--- client/login/login.js
+++ client/login/login.js
@@ -6,7 +6,7 @@ angular.module('rehjeks.login', [])
     $scope.signin = false;
     $scope.actionTitle = 'Login';
     $scope.loggedin = window.GlobalUser.username !== ''; // Shouldn't have to use this eventually
-    $scope.loggedin = document.cookie !== "undefined";
+    $scope.loggedin = document.cookie !== 'undefined';
 
 
 
@@ -19,8 +19,9 @@ angular.module('rehjeks.login', [])
     $scope.submit = function() {
       if ($scope.showLogin) {
         Auth.authorize($scope.user, '/login', $scope)
-        .then((resp)=>$scope.$parent.showDropdown=false);
+        .then((resp)=>$scope.$parent.showDropdown = false);
       } else if ($scope.showSignup) {
+
         Auth.authorize($scope.user, '/signup', $scope);
       }
       var form = document.getElementsByName('loginForm')[0];
@@ -43,7 +44,7 @@ angular.module('rehjeks.login', [])
 
     $scope.logout = function() {
       console.log('logging out');
-      document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+      document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
       window.GlobalUser.username = '';
 
       $scope.loggedin = false;
