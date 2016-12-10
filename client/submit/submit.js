9cd6626e2792fe0e6fa3f776729103bd42e42e20
Tenzin Sonam <tenso2006@gmail.com>
2016-12-09 22:37:11 -0800

    done: clear text on submission in new challenge submit form

--- client/submit/submit.js
+++ client/submit/submit.js
@@ -1,10 +1,19 @@
 angular.module('rehjeks.submit', [])
   .controller('SubmitController', function ($scope, $http, Server) {
 
+    var submitData = {};
+    //saving blank form as originForm
+    $scope.originForm = angular.copy($scope.submitData);
 
+    $scope.resetForm = function(){
+      $scope.submitData = angular.copy($scope.originForm);
+      // Assign clear state to modified form
+
+      $scope.submitForm.$setPristine();
+    };
 
     $scope.submit = function() {
-      Server.submitNewChallenge($scope, window.GlobalUser.username);
-
+      Server.submitNewChallenge($scope);
+      $scope.resetForm();
     };
   });
