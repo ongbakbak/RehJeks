195ab5dabec8b6abb888412c22534e60c14b064c
ongbakbak <ryanpsuresh@gmail.com>
2016-12-10 15:51:33 -0800

    Update factories.js

--- client/factories/factories.js
+++ client/factories/factories.js
@@ -61,7 +61,7 @@ angular.module('rehjeks.factories', [
         console.log(successRes.data.username);
         window.GlobalUser.username = successRes.data.username;
         window.GlobalUser.userId = successRes.data.userid;
-        document.cookie = `username=${successRes.data.username}; userId=${successRes.data.userid}`;
+        document.cookie = `username=${successRes.data.username}; userId=${successRes.data.userid};`;
         $scope.loggedin = true;
         return true;
       },
