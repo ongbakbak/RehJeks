6d5dac74201f2da3984d907e734ac9b00f820c04
Tenzin Sonam <tenso2006@gmail.com>
2016-12-09 17:37:19 -0800

    Feature: submit view submits challenge to database

--- client/submit/submit.js
+++ client/submit/submit.js
@@ -1,4 +1,10 @@
 angular.module('rehjeks.submit', [])
-  .controller('SubmitController', function ($scope) {
+  .controller('SubmitController', function ($scope, $http, Server) {
 
+
+
+    $scope.submit = function() {
+      Server.submitNewChallenge($scope, window.GlobalUser.username);
+
+    };
   });
