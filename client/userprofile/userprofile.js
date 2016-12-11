fdb17f76db781c668cf74562e64f484b7bd891d7
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-12-10 16:47:03 -0800

    Displaying problem breakdown on user profile

--- client/userprofile/userprofile.js
+++ client/userprofile/userprofile.js
@@ -4,9 +4,14 @@ angular.module('rehjeks.profile', [
 
   .controller('UserprofileController', function($scope, Server, $cookies) {
     $scope.user = {};
+    $scope.user.difficulties = {};
+    $scope.user.challenges = [];
+    $scope.user.difficulties.easy = 0;
+    $scope.user.difficulties.medium = 0;
+    $scope.user.difficulties.hard = 0;
     $scope.username = $cookies.get('username');
     $scope.user.show = false;
-    $scope.user.challenges = [];
+
 
     $scope.getUserChallenges = function() {
     	return Server.getUserChallenges($scope, $cookies.get('username'));
@@ -23,8 +28,18 @@ angular.module('rehjeks.profile', [
 
     $scope.getUserChallenges()
     .then(function(challenges) {
-      console.log("user challenges");
       console.log($scope.user.challenges);
+      $scope.user.challenges.forEach(function(challenge){
+        if(challenge.challenge.difficulty === 'easy'){
+          $scope.user.difficulties.easy++;
+        }
+        else if(challenge.challenge.difficulty === 'medium'){
+          $scope.user.difficulties.medium++;
+        }
+        else if(challenge.challenge.difficulty === 'hard'){
+          $scope.user.difficulties.hard++;
+        }
+      })
     });
 
   });
